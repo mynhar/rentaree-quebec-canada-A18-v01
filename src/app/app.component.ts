64e3d65a079601436b0f2380250d3c7ade03706c
Harold Bonilla Lopez <hmbonilla@gmail.com>
2026-07-04 20:08:06 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'rentaree-quebec-canada-A18-v01';
+}
